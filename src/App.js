ce3e9a1fa7477d113284df49cc7a173a41dab5e6
brocoly-dev <rivasarmando271084@icloud.com>
2024-12-14 21:49:21 -0600

    Added component folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FigureForm from './components/FigureForm/FigureForm'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <FigureForm />
       </header>
     </div>
   );
---
+++ src/components/FigureForm/FigureForm.js
@@ -0,0 +1,9 @@
+
+const FigureForm = () => (
+  <>
+    <h1>ss</h1>
+    <h2>s</h2>
+  </>
+);
+
+export default FigureForm;
