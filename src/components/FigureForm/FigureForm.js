ff48aae221c797aca873a6137137fbbae3f12d56
brocoly-dev <rivasarmando271084@icloud.com>
2024-12-18 08:12:56 -0600

    Added request to backend

--- src/components/FigureForm/FigureForm.js
+++ src/components/FigureForm/FigureForm.js
@@ -1,9 +1,26 @@
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    axios.post('http://localhost:8080/sample')
+        .then(response => console.log(response.data))
+        .catch(error => console.error('Error creating a new figure item:', error));
+};
 
 const FigureForm = () => (
-    <>
-        <h1>This is text</h1>
-        <h2>This is another text</h2>
-    </>
+    <form onSubmit={handleSubmit}>
+
+        <TextField
+            required id="outlined-basic"
+            label="Base Name" variant="outlined" helperText="Incorrect entry." size="small" />
+
+        <Button type="submit" variant="contained" color="primary">
+            Submit
+        </Button>
+
+    </form>
 );
 
 export default FigureForm;
