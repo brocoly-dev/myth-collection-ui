0d662c9f09805eb235987a845d38e81a9d2bf098
brocoly-dev <rivasarmando271084@icloud.com>
2024-12-19 07:39:54 -0600

    Added more changes

--- src/components/FigureForm/FigureForm.js
+++ src/components/FigureForm/FigureForm.js
@@ -1,26 +1,54 @@
 import { Button, TextField } from "@mui/material";
+import React, { useState } from 'react';
 import axios from "axios";
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    axios.post('http://localhost:8080/sample')
-        .then(response => console.log(response.data))
-        .catch(error => console.error('Error creating a new figure item:', error));
+
+
+
+
+const FigureForm = () => {
+    const [figurineData, setFigurineData] = useState({
+        name: '',
+        email: ''
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        /*
+        axios.post('http://localhost:8080/figurines')
+            .then(response => console.log(response.data))
+            .catch(error => console.error('Error creating a new figure item:', error));
+            */
+        try {
+            // Sending POST request with JSON data
+            const response = axios.post('http://localhost:8080/figurines', figurineData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            console.log('Response:', response.data); // handle the response as needed
+
+            // Optionally, you can reset the form or display a success message
+        } catch (error) {
+            console.error('Error creating figurine:', error.response ? error.response.data : error.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <TextField
+                required id="outlined-basic"
+                label="Base Name" variant="outlined" helperText="Incorrect entry." size="small" />
+
+            <Button type="submit" variant="contained" color="primary">
+                Submit
+            </Button>
+
+        </form>
+    );
 };
-
-const FigureForm = () => (
-    <form onSubmit={handleSubmit}>
-
-        <TextField
-            required id="outlined-basic"
-            label="Base Name" variant="outlined" helperText="Incorrect entry." size="small" />
-
-        <Button type="submit" variant="contained" color="primary">
-            Submit
-        </Button>
-
-    </form>
-);
 
 export default FigureForm;
